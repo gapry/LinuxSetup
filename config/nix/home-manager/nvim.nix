41141213aa487112212d82037e643c5ab631bd11
gapry <gapry@protonmail.com>
2026-03-11 09:50:55 +0800

    nixpkgs: add neovim

---
+++ config/nix/home-manager/nvim.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    extraPackages = with pkgs; [
+      gcc
+      gnumake
+      unzip
+      ripgrep
+      fd
+    ];
+  };
+}
