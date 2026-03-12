d43100ec834939631f16b2547704331784210427
gapry <gapry@protonmail.com>
2026-03-12 20:35:09 +0800

    ubuntu: setup nix-users group and daemon restart: fix

--- utils/ubuntu.sh
+++ utils/ubuntu.sh
@@ -8,12 +8,14 @@ sudo apt update
 echo "Installing Ubuntu packages..."
 sudo apt install -y $(cat pkgs/ubuntu/packages.txt)
 
-if ! groups $(whoami) | grep -q -w "nix-users"; then
-  echo "Adding $(whoami) to nix-users group..."
-  sudo usermod -aG nix-users $(whoami)
+if ! groups | grep -q -w "nix-users"; then
+  user=$(whoami)
 
-  echo "Restarting nix-daemon to apply group changes..."
+  sudo usermod -aG nix-users "$user"
+  echo "Added $user to nix-users group."
+
   sudo systemctl restart nix-daemon
+  echo "nix-daemon restarted. IMPORTANT: Log out and log back in for group changes to apply to your user."
 fi
 
 echo "Ubuntu setup completed."
