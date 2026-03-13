7bba5b1c66f695677110134e6fe2a7c467c72208
gapry <gapry@protonmail.com>
2026-03-13 18:36:14 +0800

    fio: src from git commit

--- config/nix/home-manager/fio.nix
+++ config/nix/home-manager/fio.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
+let
+  fio = pkgs.fio.overrideAttrs (oldAttrs: {
+    src = pkgs.fetchgit {
+      url = "https://github.com/axboe/fio.git";
+      rev = "3a0e8ddf28ad32785ea3130ce0ee42524d835d60";
+      sha256 = "05shfw2s9zjj89p5sxzdld70k2rayy2hx5aglmgrc2wi7adqz1qv";
+    };
+  });
+in
 {
   home.packages = [
-    pkgs.fio
+    fio
   ];
 }
