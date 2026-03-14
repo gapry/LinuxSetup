98c4a1d91303c1df649ff1e4bfcc45d622254c68
gapry <gapry@protonmail.com>
2026-03-14 19:46:26 +0800

    eza: define tree

--- home/.config/fish/conf.d/eza.fish
+++ home/.config/fish/conf.d/eza.fish
@@ -1,4 +1,6 @@
 if type -q eza
+  set -g EZA_IGNORE ".git|node_modules|.cache|build|result"
+
   alias ls "eza --icons --group-directories-first"
   
   alias l "ls -l"
@@ -7,4 +9,6 @@ if type -q eza
   
   alias lt "ls --tree"
   alias lx "ls -lbhHigUmuSa --time-style=long-iso --git --color-scale"
+
+  alias tree "eza -aT -I '$EZA_IGNORE'"
 end
