a88562954cb8f2f5491574474d914109c4f8bbfe
gapry <gapry@protonmail.com>
2026-03-14 19:29:20 +0800

    eza: add setting for fish (#50)

---
+++ home/.config/fish/conf.d/eza.fish
@@ -0,0 +1,10 @@
+if type -q eza
+  alias ls "eza --icons --group-directories-first"
+  
+  alias l "ls -l"
+  alias la "ls -a"
+  alias ll "ls -al"
+  
+  alias lt "ls --tree"
+  alias lx "ls -lbhHigUmuSa --time-style=long-iso --git --color-scale"
+end
