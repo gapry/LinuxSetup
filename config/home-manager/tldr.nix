94466714f279c2fbcdf3f14d9adc98be1e63dd37
gapry <gapry@protonmail.com>
2026-03-14 12:44:42 +0800

    nixpkgs: add tldr

---
+++ config/home-manager/tldr.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      display = {
+        compact = true;
+        use_pager = true;
+      };
+      updates = {
+        auto_update = true;
+      };
+    };
+  };
+}
