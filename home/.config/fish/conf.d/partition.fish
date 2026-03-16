806f4ce6881716cbfdf7a93ff6e4004378cbc0bd
gapry <gapry@protonmail.com>
2026-03-16 08:36:47 +0800

    fish: define partition_info

---
+++ home/.config/fish/conf.d/partition.fish
@@ -0,0 +1,11 @@
+if type -q lsblk
+  set -l loop_major_hex (stat -c '%t' /dev/loop0)
+
+  if test -n loop_major_hex
+    set -l loop_major_dec (math 0x$loop_major_hex)
+    alias partition_info="lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,UUID,PARTLABEL,PARTTYPENAME -e $loop_major_dec"
+  else
+    alias partition_info="lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,UUID,PARTLABEL,PARTTYPENAME"
+  end
+end
+
