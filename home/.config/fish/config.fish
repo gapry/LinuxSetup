cb438e26ac49472c6a03353422401b0aabeffdad
gapry <gapry@protonmail.com>
2026-03-19 02:36:57 +0800

    fish: move bun config to conf.d

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,11 +1,9 @@
 if status is-interactive
   fish_default_key_bindings
 
-  set -gx BUN_INSTALL "$HOME/.bun"
   set -gx OPENCODE_MODEL "codegemma:2b"
   set -gx OPENCODE_HOST "http://localhost:11434"
 
-  fish_add_path $BUN_INSTALL/bin
   fish_add_path $HOME/.opencode/bin
 
   if type -q starship
