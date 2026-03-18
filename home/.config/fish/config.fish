1b3ac3e791c86e1d58631fce7e16cd11a0fcd6ff
gapry <gapry@protonmail.com>
2026-03-19 02:45:31 +0800

    fish: move starship config to conf.d (#77)

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,9 +5,5 @@ if status is-interactive
   set -gx OPENCODE_HOST "http://localhost:11434"
 
   fish_add_path $HOME/.opencode/bin
-
-  if type -q starship
-    starship init fish | source
-  end
 end
 
