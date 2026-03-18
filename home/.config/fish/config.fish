b8ecb61b80d2434d6b8e9b22f786d6df2d26518a
gapry <gapry@protonmail.com>
2026-03-19 02:49:48 +0800

    fish: move opencode config to conf.d (#78)

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,9 +1,4 @@
 if status is-interactive
   fish_default_key_bindings
-
-  set -gx OPENCODE_MODEL "codegemma:2b"
-  set -gx OPENCODE_HOST "http://localhost:11434"
-
-  fish_add_path $HOME/.opencode/bin
 end
 
