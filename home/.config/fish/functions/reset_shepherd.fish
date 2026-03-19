d4d8aa6239a3b2cbd909fbf559d6c800551563e9
gapry <gapry@protonmail.com>
2026-03-19 16:31:49 +0800

    GNU Shepherd: add cloudflared

--- home/.config/fish/functions/reset_shepherd.fish
+++ home/.config/fish/functions/reset_shepherd.fish
@@ -1,8 +1,11 @@
 function reset_shepherd
-  systemctl --user stop shepherd
-  pkill -9 shepherd
+  systemctl --user stop shepherd 2>/dev/null
+  pkill -9 shepherd 2>/dev/null
   rm -rf /run/user/(id -u)/shepherd/
   systemctl --user daemon-reload
+  systemctl --user reset-failed shepherd
   systemctl --user start shepherd
+  systemctl --user start shepherd
+  sleep 0.5
   systemctl --user status shepherd
 end
