7f203b6f968525f8c8dba09aa0916532aed2e98d
gapry <gapry@protonmail.com>
2026-03-21 21:36:07 +0800

    nixpkgs: add navi

--- home/.config/home-manager/navi.nix
+++ home/.config/home-manager/navi.nix
@@ -1,7 +1,23 @@
 { pkgs, ... }:
 
 {
-  home.packages = [
-    pkgs.navi
-  ];
+  programs.navi = {
+    enable = true;
+
+    enableFishIntegration = true;
+
+    settings = {
+      style = {
+        tag = {
+          color = "cyan";
+        };
+      };
+      cheats = {
+        paths = [ "~/.local/share/navi/cheats/" ];
+      };
+      finder = {
+        command = "fzf";
+      };
+    };
+  };
 }
