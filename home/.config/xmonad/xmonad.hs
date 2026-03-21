2f62f16d37ef1858ffeac5698afdb7601a3fc15f
gapry <gapry@protonmail.com>
2026-03-22 06:22:38 +0800

    xmonad: xession: update

--- home/.config/xmonad/xmonad.hs
+++ home/.config/xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.SpawnOnce (spawnOnce)
 
 main :: IO ()
 main = xmonad $ docks def
@@ -11,5 +12,6 @@ main = xmonad $ docks def
     , borderWidth        = 2
     , normalBorderColor  = "#333333"
     , focusedBorderColor = "#00bbff"
+    , startupHook        = spawnOnce "alacritty"
     } `additionalKeys`
     [ ((mod4Mask, xK_p), spawn "dmenu_run") ]
