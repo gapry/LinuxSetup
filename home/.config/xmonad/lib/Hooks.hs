812aa2f13c9aa2a915c34b994b1cffc25578ff67
gapry <gapry@protonmail.com>
2026-03-22 09:33:21 +0800

    xmonad: update

--- home/.config/xmonad/lib/Hooks.hs
+++ home/.config/xmonad/lib/Hooks.hs
@@ -10,16 +10,16 @@ import Config
 import Utility
 
 defaultStartupHook = do
-  spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x002b36 --height 22"
+  spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 0 --transparent true --alpha 0 --tint 0x002b36 --height 22"
   spawnOnce "nm-applet"
   spawnOnce "volumeicon"
   spawnOn workspace1 defaultTerminal
   spawnOn workspace2 defaultBrowser
 
 defaultManageHook = composeAll
-  [ checkDock --> doIgnore
-  , manageSpawn
+  [ manageSpawn
   , manageDocks
+  , checkDock             --> doIgnore
   , isBrowser             --> doShift workspace1
   , className =? "Fcitx5" --> doIgnore
   , manageHook def
