f1a00d905d6eac73077ede7068585ebad042e26a
gapry <gapry@protonmail.com>
2026-03-23 23:49:37 +0800

    fish: add ls and update eza

---
+++ home/.config/fish/conf.d/ls.fish
@@ -0,0 +1,10 @@
+if type -q ls
+  alias l "ls -l"
+  alias la "ls -a"
+  alias ll "ls -al"
+  
+  alias lt "ls --tree"
+  alias lx "ls -lbhHigUmuSa --time-style=long-iso --git --color-scale"
+  
+  alias lrt "ls -lrt"
+end
