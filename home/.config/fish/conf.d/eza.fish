ae99b51a495fcc1aa2617d87b4747e8dd9db95f6
gapry <gapry@protonmail.com>
2026-03-23 23:55:29 +0800

    fish: add ls and update eza (#90)

--- home/.config/fish/conf.d/eza.fish
+++ home/.config/fish/conf.d/eza.fish
@@ -1,14 +1,8 @@
 if type -q eza
   set -g EZA_IGNORE ".git|node_modules|.cache|build|result"
 
-  alias ls "eza --icons --group-directories-first"
-  
-  alias l "ls -l"
-  alias la "ls -a"
-  alias ll "ls -al"
-  
-  alias lt "ls --tree"
-  alias lx "ls -lbhHigUmuSa --time-style=long-iso --git --color-scale"
-
-  alias tree "eza -aT -I '$EZA_IGNORE'"
+  alias ezag "eza --icons --group-directories-first"
+  alias ezax "eza -lbhHigUmuSa --time-style=long-iso --git --color-scale"
+  alias ezat "eza --tree"
+  alias ezati "eza -aT -I '$EZA_IGNORE'"
 end
---
+++ home/.config/fish/conf.d/ls.fish
@@ -0,0 +1,6 @@
+if type -q ls
+  alias l "ls -l"
+  alias la "ls -a"
+  alias ll "ls -al"
+  alias lrt "ls -lrt"
+end
