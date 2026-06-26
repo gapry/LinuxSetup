aab42909f451da73ba850678c6be57e99e03ddb2
gapry <gapry@protonmail.com>
2026-06-26 18:35:55 +0800

    android.fish: fix typo

--- home/.config/fish/conf.d/android.fish
+++ home/.config/fish/conf.d/android.fish
@@ -6,6 +6,6 @@ if test -d "$HOME/Android/Sdk"
   set -gx ANDROID_SDK_ROOT "$HOME/Android/Sdk"
   set -gx ANDROID_HOME "$ANDROID_SDK_ROOT" # https://developer.android.com/tools
   
-  fish_add_path "ANDROID_SDK_ROOT/emulator"
+  fish_add_path "$ANDROID_SDK_ROOT/emulator"
   fish_add_path "$ANDROID_SDK_ROOT/platform-tools"
 end
