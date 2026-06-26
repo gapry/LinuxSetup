072637f7ba4e775541ab13dc152182a6fbed36f9
gapry <gapry@protonmail.com>
2026-06-26 18:17:32 +0800

    fish: configure Android SDK and Studio paths

---
+++ home/.config/fish/conf.d/android.fish
@@ -0,0 +1,11 @@
+if test -d $HOME/Android/android-studio
+  fish_add_path $HOME/Android/android-studio/bin
+end
+
+if test -d $HOME/Android/Sdk
+  set -gx ANDROID_SDK_ROOT $HOME/Android/Sdk
+  set -gx ANDROID_HOME $ANDROID_SDK_ROOT
+  
+  fish_add_path $ANDROID_SDK_ROOT/emulator
+  fish_add_path $ANDROID_SDK_ROOT/platform-tools
+end
