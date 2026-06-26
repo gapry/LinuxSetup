a8c64c9967b0153cc0b20c2704d9b3d021a8283f
gapry <gapry@protonmail.com>
2026-06-26 18:33:48 +0800

    android.fish: add double quotes

--- home/.config/fish/conf.d/android.fish
+++ home/.config/fish/conf.d/android.fish
@@ -1,11 +1,11 @@
-if test -d $HOME/Android/android-studio
-  fish_add_path $HOME/Android/android-studio/bin
+if test -d "$HOME/Android/android-studio"
+  fish_add_path "$HOME/Android/android-studio/bin"
 end
 
-if test -d $HOME/Android/Sdk
-  set -gx ANDROID_SDK_ROOT $HOME/Android/Sdk
-  set -gx ANDROID_HOME $ANDROID_SDK_ROOT # https://developer.android.com/tools
+if test -d "$HOME/Android/Sdk"
+  set -gx ANDROID_SDK_ROOT "$HOME/Android/Sdk"
+  set -gx ANDROID_HOME "$ANDROID_SDK_ROOT" # https://developer.android.com/tools
   
-  fish_add_path $ANDROID_SDK_ROOT/emulator
-  fish_add_path $ANDROID_SDK_ROOT/platform-tools
+  fish_add_path "ANDROID_SDK_ROOT/emulator"
+  fish_add_path "$ANDROID_SDK_ROOT/platform-tools"
 end
