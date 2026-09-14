61970bd591ca4a813f208624e5ea4d75b96ac488
gapry <gapry@protonmail.com>
2026-09-14 13:06:41 +0800

    fzf: switch fetchgit from ssh to https (#109)

--- home/.config/home-manager/fzf.nix
+++ home/.config/home-manager/fzf.nix
@@ -3,7 +3,7 @@
 let
   fzf-git = pkgs.fzf.overrideAttrs (oldAttrs: {
     src = pkgs.fetchgit {
-      url = "git@github.com:junegunn/fzf.git";
+      url = "https://github.com/junegunn/fzf.git";
       rev = "eacef5ea6e39c6be3fff4e231fc6d10ba2ff9491";
       sha256 = "0wginamnlp8cs0i54iwhlmwkaswfi8fhkm26bczafzrki31pc6kb";
     };
