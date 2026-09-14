974c682f4359ff37eaaf0dd2e29c843c112ecfb1
gapry <gapry@protonmail.com>
2026-09-14 12:55:24 +0800

    flake: add nixgl input and allow nvidia unfree pkg

--- home/.config/home-manager/flake.nix
+++ home/.config/home-manager/flake.nix
@@ -5,18 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  outputs = { nixpkgs, nixpkgs-unstable, nixgl, home-manager, ... }:
     let
       system = "x86_64-linux";
 
       pkgs = import nixpkgs {
         inherit system;
+
+        config.allowUnfreePredicate = pkg: nixpkgs.lib.getName pkg == "nvidia";
 
         overlays = [
           (final: prev: {
@@ -33,7 +40,7 @@
     {
       homeConfigurations."${userConfig.username}" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        extraSpecialArgs = { inherit userConfig; };
+        extraSpecialArgs = { inherit userConfig nixgl; };
         modules = [ ./home.nix ];
       };
     };
